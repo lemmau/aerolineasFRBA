f5cce0b3957ad4384c7770e47030d0a215ff04b1
Leandro Mauro <lemmau@hotmail.com>
2015-11-03 16:25:09 -0300

    script -ahora- optimizado

---
+++ sql/random.sql
@@ -0,0 +1,305 @@
+CREATE PROCEDURE [HAY_TABLA].[sp_get_rol_by_id]
+	@id int
+AS
+BEGIN
+	SET NOCOUNT ON;
+
+	SELECT 
+		Id,
+		Nombre,
+		Status
+	FROM 
+		[HAY_TABLA].Rol
+	WHERE
+		Id = @id
+END
+GO
+
+CREATE PROCEDURE [HAY_TABLA].[sp_insertar_rol]
+	@nombre NVARCHAR(50),
+	@estado bit
+AS
+BEGIN
+	
+	if (exists(select id from [HAY_TABLA].ROL where NOMBRE = @nombre))
+		begin
+			RAISERROR(N'Ya existe un Rol con ese nombre',16,1)
+			return
+		end		
+		
+	INSERT INTO [HAY_TABLA].ROL  (NOMBRE, STATUS)
+    OUTPUT
+		inserted.id
+    VALUES
+          (@nombre, @estado)
+END
+GO
+
+CREATE PROCEDURE [HAY_TABLA].[sp_baja_rol]
+	@id	int
+AS
+BEGIN
+	SET NOCOUNT ON;
+	UPDATE 
+		[HAY_TABLA].ROL
+	SET 
+		STATUS = 0
+	WHERE
+		id = @id
+END
+GO
+
+CREATE PROCEDURE [HAY_TABLA].[sp_select_roles]
+	@nombre nvarchar(50)
+AS
+BEGIN
+	SET NOCOUNT ON;
+
+	SELECT
+		Id,
+		Nombre,
+		Status
+	FROM	
+		[HAY_TABLA].Rol
+	WHERE		
+		((@nombre is null) or Nombre like '%' + @nombre + '%')
+	ORDER BY
+		Nombre 
+END
+GO
+
+CREATE PROCEDURE [HAY_TABLA].[sp_get_usuario_by_id]
+	@id int
+AS
+BEGIN
+	SET NOCOUNT ON;
+
+	SELECT ID, USERNAME, PASSWORD, INTENTOSFALLIDOS
+	FROM [HAY_TABLA].USUARIO
+	WHERE USUARIO.Id = @id
+END
+GO
+
+CREATE PROCEDURE [HAY_TABLA].[sp_get_usuario_by_login]
+	@username NVARCHAR(255),
+	@password NVARCHAR(256)
+AS
+BEGIN
+	SET NOCOUNT ON;
+
+	SELECT ID, USERNAME, PASSWORD, INTENTOSFALLIDOS
+	FROM [HAY_TABLA].USUARIO
+	WHERE USERNAME = @username AND PASSWORD = @password
+END
+GO
+
+CREATE PROCEDURE [HAY_TABLA].[sp_get_usuario_intentos]
+	@username NVARCHAR(255)
+AS
+BEGIN
+	SET NOCOUNT ON;
+
+	SELECT INTENTOSFALLIDOS
+	FROM [HAY_TABLA].USUARIO
+	WHERE USERNAME = @username
+END
+GO
+
+CREATE PROCEDURE [HAY_TABLA].[sp_set_usuario_intentos]
+	@username NVARCHAR(255),
+	@intentos int
+AS
+BEGIN
+	SET NOCOUNT ON;
+
+	UPDATE [HAY_TABLA].USUARIO 
+	SET INTENTOSFALLIDOS = @intentos 
+	WHERE USERNAME = @username
+END
+GO
+
+CREATE PROCEDURE [HAY_TABLA].[sp_modificacion_rol]
+	@id	int,
+	@nombre nvarchar(50),
+	@estado bit
+AS
+BEGIN
+	SET NOCOUNT ON;
+	
+		if (exists(select id from [HAY_TABLA].ROL where Nombre = @nombre and id<> @id))
+		begin
+			RAISERROR(N'Ya existe un Rol con ese nombre',16,1)
+			return
+		end		
+	
+	UPDATE 
+		[HAY_TABLA].ROL
+	SET 
+		Nombre = @nombre,
+		Status = @estado
+	WHERE
+		id = @id
+END
+GO
+
+-----------------------
+CREATE PROCEDURE [HAY_TABLA].[sp_select_funcionalidades_de_rol]
+	@rol_id int,
+	@nombre varchar(255) = null
+AS
+BEGIN
+	SET NOCOUNT ON;
+
+	SELECT 
+		 f.Id,
+		 f.Nombre,
+		 ISNULL((
+			SELECT
+				1
+			FROM 
+				FUNCIONALIDAD_ROL fr 
+			WHERE
+				fr.ID_FUNCIONALIDAD = f.id and 
+				fr.ID_ROL = @rol_id), 0) AS 'Seleccionado'
+	FROM
+		[HAY_TABLA].FUNCIONALIDAD f
+	WHERE
+		(@nombre is null) or (@nombre = f.Nombre)
+	ORDER BY
+		Nombre
+END
+GO
+
+--- =============================================
+--- Description:	Devuelve un listado de funcionalidades de un rol nuevo
+--- =============================================
+CREATE PROCEDURE [HAY_TABLA].[sp_select_funcionalidades_de_rol_nuevo]
+AS
+BEGIN
+	SET NOCOUNT ON;
+
+	SELECT 
+		 f.Id,
+		 f.Nombre,
+		 0 as 'Seleccionado'
+	FROM
+		[HAY_TABLA].FUNCIONALIDAD f
+	ORDER BY
+		Nombre
+END
+GO
+
+CREATE PROCEDURE [HAY_TABLA].[sp_insertar_funcionalidad_a_rol]
+	@idRol int,
+	@idFuncionalidad int
+AS
+BEGIN
+	SET NOCOUNT ON;
+
+	INSERT INTO [HAY_TABLA].[FUNCIONALIDAD_ROL]
+           ([ID_ROL]
+           ,[ID_FUNCIONALIDAD])
+     VALUES
+           (@IdRol
+           ,@IdFuncionalidad)
+END
+GO
+
+CREATE PROCEDURE [HAY_TABLA].[sp_borrar_funcionalidades_de_rol]
+       @idRol int 
+AS
+BEGIN
+	SET NOCOUNT ON;
+
+	DELETE FROM
+		[HAY_TABLA].FUNCIONALIDAD_ROL
+	WHERE
+		ID_ROL = @idRol
+END
+GO
+
+CREATE PROCEDURE [HAY_TABLA].[sp_select_roles_de_usuario]
+	@id_usuario int
+AS
+BEGIN
+	SET NOCOUNT ON;
+
+	SELECT
+		ID_ROL
+	FROM	
+		[HAY_TABLA].ROLES_USUARIO
+	WHERE
+		ID_USUARIO = @id_usuario
+END
+GO
+
+CREATE PROCEDURE [HAY_TABLA].[sp_get_ciudades]
+AS
+BEGIN
+	SET NOCOUNT ON;
+
+	SELECT 
+		ID, NOMBRE, STATUS
+	FROM 
+		[HAY_TABLA].CIUDAD
+	WHERE
+		STATUS = 1
+	ORDER BY
+		NOMBRE
+END
+GO
+
+CREATE PROCEDURE [HAY_TABLA].[sp_insertar_ciudad]
+	@nombre nvarchar(100)
+AS
+BEGIN
+	SET NOCOUNT ON;
+	if (exists(select id from [HAY_TABLA].CIUDAD where NOMBRE like '%' + @nombre and STATUS = 1))
+		begin
+			RAISERROR(N' Ya existe una ciudad con ese nombre ', 16, 1)
+			return
+		end		
+	else 
+		begin
+			if (exists(select id from [HAY_TABLA].CIUDAD where NOMBRE like '%' + @nombre and STATUS = 0))		
+				begin
+					UPDATE 
+						[HAY_TABLA].CIUDAD
+					SET 
+						STATUS = 1
+					WHERE
+						NOMBRE LIKE '%' + @nombre 
+						AND STATUS = 0
+					RAISERROR(N' Habilito Ciudad ', 16, 1)
+					return
+				end
+			else
+				begin
+					INSERT INTO [HAY_TABLA].CIUDAD 
+								(NOMBRE, STATUS)
+					OUTPUT
+						inserted.id
+					VALUES
+						(@nombre, 1)
+					RAISERROR(N' Agrego Nueva Ciudad ', 16, 1)
+					return
+				end
+		end
+END
+GO
+
+CREATE PROCEDURE [HAY_TABLA].[sp_eliminar_ciudad]
+	@id int
+AS
+BEGIN
+	SET NOCOUNT ON;
+
+    UPDATE 
+		[HAY_TABLA].CIUDAD
+	SET 
+		STATUS = 0
+	WHERE
+		ID = @id
+END
+GO
+
